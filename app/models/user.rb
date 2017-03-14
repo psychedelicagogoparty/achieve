0113acec08cca2d70647b214d56a3374d83c894f
Psychedelicagogoparty <yosi@Yoshimitsu-no-MacBook-Air.local>
2017-03-14 21:48:29 +0900

    dive 12 ver3.0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   #以下のメソッドhas_many :hoges により子モデル（hoge）が複数紐づくアソシエーションを定義
-  has_many :blogs, dependent: destroy
+  has_many :blogs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
